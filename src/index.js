cbc58219fc2ff11b5911c9ab1a9bceadd5386d7b
Michael Friedman <contact@michaeldavidfriedman.com>
2017-07-22 22:12:44 -0700

    Video thumbnails and headers successfully rendering from YouTube API response

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 
 const API_KEY = 'AIzaSyAuyOZobjd3G0I66Dx7d4p7MPJOHib6x3o';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      videos: [],
+    };
+
+    YTSearch({
+      key: API_KEY,
+      term: 'surfboards',
+    }, videos => this.setState({ videos }));
   }
   render() {
+    const { videos } = this.state;
     return (
       <div>
         <SearchBar />
+        <VideoList videos={videos} />
       </div>
     );
   }
